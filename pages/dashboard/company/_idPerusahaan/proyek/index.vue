50843f14162b949a922d81cf03178a38bac8a2a0
fatkulnurk <fatkulnurk@gmail.com>
2019-11-27 17:02:36 +0700

    update company dan investor

--- pages/dashboard/company/_idPerusahaan/proyek/index.vue
+++ pages/dashboard/company/_idPerusahaan/proyek/index.vue
@@ -3,21 +3,21 @@
     <v-container grid-list-xl fluid>
       <v-layout row wrap>
 
-        <v-flex lg4 sm12>
+        <v-flex lg4 sm12 v-for="item in this.proyeks">
           <v-card>
-            <v-card-media :src="require('@/static/bg/8.jpg')" height="250">
-              <h2 class="white--text pa-3">
-                Card with image
-              </h2>
-            </v-card-media>
+            <v-card-media :src="require('@/static/bg/8.jpg')" height="250"></v-card-media>
+            <v-card-text>
+              <h3 class="headline">Nama Proyek</h3>
+            </v-card-text>
+            <v-divider></v-divider>
             <v-card-text>
               <div>
-                Lorem ipsum
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
               </div>
             </v-card-text>
             <v-divider></v-divider>
             <v-card-actions>
-              <v-btn flat small @click="detailProyek">Info Lebih Lanjut</v-btn>
+              <v-btn flat small @click="listProyek">Info Lebih Lanjut</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -29,12 +29,19 @@
 
 <script>
   import Material from 'vuetify/es5/util/colors';
-  
+
   export default {
     layout: 'dashboard',
     data: () => ({
       color: Material,
-      selectedTab: 'tab-1'
+      selectedTab: 'tab-1',
+      proyeks: [
+        '',
+        '',
+        '',
+        '',
+        ''
+      ]
     }),
 
     methods: {
--- pages/dashboard/company/index.vue
+++ pages/dashboard/company/index.vue
@@ -2,16 +2,16 @@
   <div id="pageDashboard">
     <v-container grid-list-xl fluid>
       <v-layout row wrap>
-        <v-flex lg4 sm12>
+        <v-flex lg4 sm12 v-for="item in this.companys">
           <v-card>
-            <v-card-media :src="require('@/static/bg/8.jpg')" height="250">
-              <h2 class="white--text pa-3">
-                Card with image
-              </h2>
-            </v-card-media>
+            <v-card-media :src="require('@/static/bg/8.jpg')" height="250"></v-card-media>
+            <v-card-text>
+              <h3 class="headline">Nama Perusahaan</h3>
+            </v-card-text>
+            <v-divider></v-divider>
             <v-card-text>
               <div>
-                Lorem ipsum
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
               </div>
             </v-card-text>
             <v-divider></v-divider>
@@ -20,7 +20,6 @@
             </v-card-actions>
           </v-card>
         </v-flex>
-
       </v-layout>
     </v-container>
   </div>
@@ -33,7 +32,16 @@
     layout: 'dashboard',
     data: () => ({
       color: Material,
-      selectedTab: 'tab-1'
+      selectedTab: 'tab-1',
+      companys: [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+      ]
     }),
 
     methods: {
