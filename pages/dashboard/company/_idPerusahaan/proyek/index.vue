4479e6351e86405229f311b6cdc4c044772f0413
fatkulnurk <fatkulnurk@gmail.com>
2019-11-27 22:11:47 +0700

    create modal in create proyek

--- pages/dashboard/company/_idPerusahaan/proyek/index.vue
+++ pages/dashboard/company/_idPerusahaan/proyek/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="pageDashboard">
     <v-container grid-list-xl fluid>
+      <v-btn color="primary" block @click="showPopupAddProject">Tambah Proyek</v-btn>
+
       <v-layout row wrap>
 
         <v-flex lg4 sm12 v-for="item in this.proyeks">
@@ -24,6 +26,62 @@
 
       </v-layout>
     </v-container>
+    <modal name="add-project"
+           transition="nice-modal-fade"
+           classes="demo-modal-class"
+           :min-width="200"
+           :min-height="200"
+           :pivot-y="0.5"
+           :adaptive="true"
+           :scrollable="true"
+           :reset="true"
+           width="60%"
+           height="auto">
+
+      <v-card>
+        <v-card-text>
+          <v-flex sm-12>
+            <div align="center">
+              <h3 class="title">Tambah Proyek</h3>
+            </div>
+          </v-flex>
+          <v-form>
+            <div class="d-flex my-2">
+              <v-text-field
+                label="Nama Proyek"
+                v-validate="'required'"
+                :error-messages="errors.collect('nama_proyek')"
+                required
+              ></v-text-field>
+            </div>
+
+            <v-flex lg12 sm12>
+              <v-text-field textarea label="Description">
+              </v-text-field>
+            </v-flex>
+
+            <v-text-field
+              label="Prospektus"
+              type="file"
+            ></v-text-field>
+
+            <v-text-field
+              label="Target"
+            ></v-text-field>
+
+            <v-text-field
+              label="Gambar"
+              type="file"
+            ></v-text-field>
+
+            <div class="form-btn" >
+              <v-btn color="primary" class="btn-margin-bottom" block>Submit</v-btn>
+              <v-btn color="error" block>Clear</v-btn>
+            </div>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </modal>
   </div>
 </template>
 
@@ -48,8 +106,17 @@
       detailProyek() {
         let id = 1;
         this.$router.push('proyek/' + id);
+      },
+      showPopupAddProject() {
+        this.$modal.show('add-project');
       }
     },
 
   };
 </script>
+
+<style scoped>
+  .btn-margin-bottom {
+    margin-bottom: 10px;
+  }
+</style>
