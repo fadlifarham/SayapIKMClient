58abfef3c248fc8269f7765cb6aa0367597f3c77
fatkulnurk <fatkulnurk@gmail.com>
2019-11-27 21:03:06 +0700

    create vue modal

---
+++ components/modal/modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <modal name="hello-world">
+    hello, world!
+  </modal>
+</template>
+
+<script>
+    export default {
+      name: "modal",
+      methods: {
+        show () {
+          this.$modal.show('hello-world');
+        },
+        hide () {
+          this.$modal.hide('hello-world');
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
